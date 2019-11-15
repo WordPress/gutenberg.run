12fbf6828a1795112a927ed0384108ef78f8581b
Andrew Duthie <andrew@andrewduthie.com>
2019-11-15 11:07:57 -0500

    Filter expired containers of any tag
    "Ancestor" filter does not appear to produce results for all tags, and while specifiying individual tag is possible, wildcard tag filtering is not supported as far as my testing seems to demonstrate

--- images/gutenbergrun-cleaner/purge-containers.sh
+++ images/gutenbergrun-cleaner/purge-containers.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-create_cutoff=$(date -d "-$CONTAINER_TTL_SECONDS seconds" +"%Y-%m-%d %H:%M:%S %z %Z" )
+create_cutoff=$(date -d "-$CONTAINER_TTL_SECONDS seconds" +"%Y-%m-%d %H:%M:%S %z %Z")
 echo "Running clean, removing any container older than $create_cutoff"
 
 #============================
 # Remove expired containers
 #============================
 
+# Filter containers by those matching image (of _any tag_) and creation date
+# exceeding allowable start cutoff.
 expired=$(
-	docker ps -a --filter "ancestor=gutenbergrun-site" --format='{{.ID}},{{.Names}},{{.CreatedAt}}' \
-		| awk -v create_cutoff="$create_cutoff" -F ',' '$3 < create_cutoff {printf "%s,%s\n", $1, $2}'
+	docker ps -a --format='{{.Image}},{{.CreatedAt}},{{.ID}},{{.Names}}' \
+		| awk -v create_cutoff="$create_cutoff" -F ',' '$1 ~ /^gutenbergrun-site:/ && $2 < create_cutoff {printf "%s,%s\n", $3, $4}'
 )
 
 for pair in $expired
